a3f86c7a4dcd2190f4af8878c656421fd8e0ce41
dong.yang <dong.yang@mthreads.com>
2023-07-10 12:46:22 +0800

    porting stable diffusion to musa success

---
+++ webui_musa.sh
@@ -0,0 +1,4 @@
+TORCH_COMMAND="" python webui.py --no-half --listen --port 8000
+# pip config set global.index-url https://pypi.org/simple
+# export TORCH_COMMAND="pip install six" && bash webui.sh -f --skip-torch-cuda-test --no-half --listen --port 8000
+# export TORCH_COMMAND="pip install six" && bash webui.sh -f --skip-torch-cuda-test --enable-insecure-extension-access --no-half --listen --port 8000
